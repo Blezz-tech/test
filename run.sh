7660bbc60e0cacb9b4d3fe7e1d5b0ee132896d6a
Blezz-tech <markus.jenya04@yandex.ru>
2024-08-17 23:02:35 +0300

    asd

--- run.sh
+++ run.sh
@@ -2,15 +2,20 @@ cd $1
 
 files=$(find . -maxdepth 1 -type f)
 
+folders=$(find . -type d)
+
 counter=$(date +%s)
 
 echo $counter
 
-for file in $files; do
-    extension="${file##*.}"
-    name="${file%.*}"
-    mv $file "$name $counter.$extension"
-    ((counter+=1))
+for folder in $folders; do
+    # cd $folder
+    echo $folder
+    # for file in $files; do
+    #     extension="${file##*.}"
+    #     name="${file%.*}"
+    #     mv $file "$name $counter.$extension"
+    #     ((counter+=1))
+    # done
 done
-
 
